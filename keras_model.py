2816d3aff6e6c36bab7b1edc3f9d90f7d775c044
Uki Malla <uki.malla@gmail.com>
2018-07-28 14:01:03 -0700

    Keras sample complete

--- keras_model.py
+++ keras_model.py
@@ -1,6 +1,6 @@
 import keras
 from keras import Sequential
-from keras.layers import Dense, Dropout, Conv1D, MaxPool1D, LSTMCell, Flatten
+from keras.layers import Dense, Dropout, Conv1D, MaxPool1D, LSTM, Flatten, Reshape
 import pandas as pd
 import math
 from preprocessing import clean_data, generate_key_map
@@ -34,7 +34,7 @@ n_step = CHUNK  # the length of the input sequence
 # n_input = n_class = v_len  # the size of each input
 n_input = 1
 n_class = v_len
-batch_size = 150
+batch_size = 1
 
 
 """
@@ -49,24 +49,21 @@ number_of_batches = int(math.ceil(len(X_train) / batch_size))
 Model
 """
 
-
 model = Sequential()
-model.add(Conv1D(filters=32, kernel_size=30, strides=1, padding="causal", input_shape=(512, 1)))
+model.add(Conv1D(filters=1, kernel_size=5, strides=1, padding="causal", input_shape=(512, 1)))
 model.add(MaxPool1D(pool_size=2))
 model.add(Flatten())
+model.add(Reshape((256, 1)))
 model.add(Dropout(0.25))
-model.add(LSTMCell(512, dropout=0.25))
+model.add(LSTM(128, dropout=0.25))
+
 model.add(Dense(n_class, activation="softmax"))
 
-
-model.compile(optimizer=keras.optimizers.adam, loss=keras.losses.sparse_categorical_crossentropy(
-    y_true=y_train, y_pred=model.predict(X_train)))
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy")
 
 model.fit(x=X_train, y=y_train, batch_size=batch_size, epochs=100)
 
-
-
-
+model.save("model.h5")
 
 
 
